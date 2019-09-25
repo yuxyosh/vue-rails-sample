4170d3c1e1ae3804b98363a36618a8885fb6964b
Yoshioka <sss.yoshioka@gmail.com>
2019-09-25 15:17:11 +0900

    render file:を整備

--- rails/app/controllers/vue_controller.rb
+++ rails/app/controllers/vue_controller.rb
@@ -1,4 +1,4 @@
-class VueController < ApplicationController
+class VueController < ActionController::Base
   def vue_transfer
     # response.headers['Service-Worker-Allowed'] = '/'
     render file: 'public/vue/index.html', layout: false
